b3a5420c644047547d6aa120a0bfd596304aba0d
Kevin Han <yd.han181@gmail.com>
2019-05-20 14:01:58 -0400

    Added media controls to navigation.

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,38 +1,77 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getRecommendations } from '../store/actions'
-import { Menu, Icon } from 'antd'
+import { Menu, Icon, Avatar } from 'antd'
+
+const styles = {
+    iconSize1: { fontSize: '18px' },
+    iconSize2: { fontSize: '20px' },
+    left: { float: 'left' },
+    right: { float: 'right' },
+    paddingLeft: { paddingLeft: '1em' },
+}
 
 const NavBar = props => {
     const { openDrawer, getRecommendations } = props
 
     return (
-        <Menu mode='horizontal' selectedKeys={[]}>
-            <Menu.Item key='playlists-menu' onClick={openDrawer}>
-                <Icon type='menu' />
+        <Menu
+            mode='horizontal'
+            selectedKeys={[]}
+            style={{ textAlign: 'center' }}
+        >
+            {/* Toggle playlist drawer button */}
+            <Menu.Item
+                key='playlists-menu'
+                style={styles.left}
+                onClick={openDrawer}
+            >
+                <Icon type='menu' style={styles.iconSize1} />
             </Menu.Item>
 
-            <Menu.Item key='get-recommendations' onClick={getRecommendations}>
-                <Icon type='sync' />
+            {/* Go to exclusions page button */}
+            <Menu.Item key='manage-exclusions' style={styles.left}>
+                <Icon type='stop' style={styles.iconSize1} />
             </Menu.Item>
 
-            <Menu.Item key='manage-exclusions'>
-                <Icon type='stop' />
+            {/* Media control buttons*/}
+            <Menu.Item>
+                <Icon type='fast-backward' style={styles.iconSize1} />
+            </Menu.Item>
+
+            <Menu.Item>
+                <Icon type='caret-right' style={styles.iconSize1} />
+            </Menu.Item>
+
+            <Menu.Item>
+                <Icon type='fast-forward' style={styles.iconSize1} />
+            </Menu.Item>
+
+            {/* Album cover */}
+            <Menu.Item>
+                <Avatar shape='square' icon='user' />
+                <span style={styles.paddingLeft}>
+                    Current Song - Current Artist
+                </span>
+            </Menu.Item>
+
+            {/* Get recommendations button */}
+            <Menu.Item
+                key='get-recommendations'
+                style={styles.right}
+                onClick={getRecommendations}
+            >
+                <Icon type='sync' style={styles.iconSize1} />
             </Menu.Item>
         </Menu>
     )
 }
-
-const mapStateToProps = ({ tracks, recommendations }) => ({
-    tracks,
-    recommendations,
-})
 
 const mapDispatchToProps = (dispatch, props) => ({
     getRecommendations: () => dispatch(getRecommendations()),
 })
 
 export default connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
 )(NavBar)
