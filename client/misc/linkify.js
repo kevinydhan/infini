b25d4ced83efe43aa83e3c9f8c69b83d93e91061
Kevin Han <yd.han181@gmail.com>
2019-06-22 23:48:18 -0400

    Documented utility JSX render functions

--- client/misc/linkify.js
+++ client/misc/linkify.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+/**
+ * Renders an `<a>` element with the `href` directing to specified url. The link color defaults to black.
+ * @param {string} text - Display text
+ * @param {string} url - Href for `<a>` element
+ */
+
 const linkify = (text, url) => (
     <a href={url} style={styles.link} target="_blank">
         {text}
