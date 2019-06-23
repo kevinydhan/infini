070db62fd7d73fdd378c00679964ace26bce55fa
Kevin Han <yd.han181@gmail.com>
2019-06-22 20:01:39 -0400

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    devtool: 'eval',
-    entry: './client',
-    output: {
-        path: __dirname,
-        filename: 'public/bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  devtool: 'eval',
+  entry: './client',
+  output: {
+    path: __dirname,
+    filename: 'public/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
