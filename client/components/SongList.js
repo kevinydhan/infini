40a5b67327894f381a525cb0af15f57131030b15
Kevin Han <yd.han181@gmail.com>
2019-07-11 00:44:42 -0400

    Reverting back to old layout

--- client/components/SongList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SongList = props => {
-    return <div>hi</div>
-}
-
-export default SongList
--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 // React imports
-import React, { Fragment, Component } from 'react'
+import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 // Redux imports
@@ -9,32 +9,11 @@ import { getMe } from './store/actions'
 
 // Subcomponent imports
 import Landing from './components/Landing'
-import SideBar from './components/SideBar'
-import NavBar from './components/NavBar'
-import SongList from './components/SongList'
-
-import { Layout } from 'antd'
+import Navigation from './components/Navigation'
 import 'antd/dist/antd.css'
 
 class App extends Component {
-    state = {
-        isLoggedIn: false,
-        pageIndex: 1,
-        playlistId: '',
-        isSideBarCollapsed: false
-    }
-
-    handlePageChange = event => {
-        const params = event.key.split('--')
-        const updatedState = { pageIndex: +params[0], playlistId: '' }
-
-        if (params.length === 2) updatedState.playlistId = params[1]
-
-        this.setState({ ...updatedState })
-    }
-
-    toggleSideBar = () =>
-        this.setState({ isSideBarCollapsed: !this.state.isSideBarCollapsed })
+    state = { isLoggedIn: false }
 
     async componentDidMount() {
         const user = await this.props.getMe()
@@ -42,49 +21,12 @@ class App extends Component {
     }
 
     render() {
-        const { isLoggedIn, isSideBarCollapsed } = this.state
-        const { pageIndex, playlistId } = this.state
-
+        const { isLoggedIn } = this.state
         if (!isLoggedIn) return <Landing />
-
         return (
-            <Fragment>
-                <Layout>
-                    {/* Render side bar */}
-                    <Layout.Sider
-                        trigger={null}
-                        collapsed={isSideBarCollapsed}
-                        style={{
-                            overflow: 'auto',
-                            height: '100vh',
-                            position: 'fixed',
-                            left: 0
-                        }}
-                    >
-                        <SideBar />
-                    </Layout.Sider>
-
-                    <Layout
-                        style={{
-                            marginLeft: isSideBarCollapsed ? '80px' : '200px',
-                            padding: 0
-                        }}
-                    >
-                        {/* Render top nav bar */}
-                        <Layout.Header>
-                            <NavBar
-                                isSideBarCollapased={isSideBarCollapsed}
-                                toggleSideBar={this.toggleSideBar}
-                            />
-                        </Layout.Header>
-
-                        {/* Render content */}
-                        <Layout.Content>
-                            <SongList />
-                        </Layout.Content>
-                    </Layout>
-                </Layout>
-            </Fragment>
+            <div>
+                <Navigation />
+            </div>
         )
     }
 }
